374c2dbcc93a4c167639e3f33555464f9a449209
Manish Chaudhari <chaudharimanish07@gmail.com>
2023-08-01 23:51:01 -0400

    finished styling

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react'
+import MovieDisplay from './components/MovieDisplay'
+import Form from './components/Form'
+
+
+
+
+const containerStyle = {
+  height: '100%',
+  backgroundColor: "white",
+  margin: "0 auto",
+  maxWidth: "1200px",
+  padding: "20px" 
+} 
+
+const innerContainer = {
+  display: 'flex',
+  background: 'white',
+  flexDirection: 'column',
+  height: '100%',
+  justifyContent: 'center'
+}
+
+function App() {
+  const apiKey = 'e5e62db7'
+  const [movie, setMovie] = useState(null)
+
+  const getMovie = async (searchTerm) => {
+
+    try {
+      const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`)
+      const data = await response.json();
+      setMovie(data)
+
+    } catch (error) {
+      console.log(error);
+
+    }
+
+  }
+
+   useEffect(()=>{
+    getMovie('shrek')
+   },[])
+
+  return (
+
+    <div style ={containerStyle}>
+      <Form movieSearch={getMovie} />
+      <div style={innerContainer}>
+        <MovieDisplay movie={movie} />
+      </div>
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/MovieDisplay.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 3em;
+  letterSpacing: 2px;
+  color: white;
+`
+const Genre = styled.h2`
+  font-size: 2em;
+  color: white;
+`
+const movieDisplayStyle = {
+    background: 'black',
+    border: '1px solid black',
+    
+    minWidth: "800px",
+    textAlign: "center"
+}
+
+function MovieDisplay({ movie }) {
+    const loaded = () => {
+        return (
+            <div style={movieDisplayStyle}>
+                <Title>{movie.Title}</Title>
+                <Genre>{movie.Genre}</Genre>
+        
+                <img src={movie.Poster} alt={movie.Title} />
+                <h2>{movie.Year}</h2>
+            </div>
+        );
+    };
+
+    const loading = () => {
+        return <h1>No movie to display</h1>
+
+    }
+
+    return movie ? loaded() : loading()
+ß
+}
+
+export default MovieDisplay
